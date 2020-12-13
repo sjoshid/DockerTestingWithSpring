9bdfe1d8fcb873e75af57cf252e28e67d5937df6
Sujit Joshi <joshi.sujit020386@gmail.com>
2020-12-12 21:43:08 -0500

    Initial commit.

---
+++ src/com/sj/SampleController.java
@@ -0,0 +1,15 @@
+package com.sj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String showHelloWorld() {
+        return "Hello, world";
+    }
+}
